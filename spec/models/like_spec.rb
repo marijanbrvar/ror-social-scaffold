b71f7337782d2470d754387c01aac1be5ab807a2
marijanbrvar <marijan.brvar@gmail.com>
2021-04-20 20:55:28 +0200

    Add model tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+
+	describe 'Validations' do
+    it { should validate_uniqueness_of(:user_id).scoped_to(:post_id) }
+  end
+  
   it { should belong_to :user }
   it { should belong_to :post }
 end
