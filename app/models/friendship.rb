04c75a43ab9f2c234dc156215693f6c73ba42afc
marijanbrvar <marijan.brvar@gmail.com>
2021-04-21 10:12:05 +0200

    Add double row firendship in models and controller

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,11 @@ class Friendship < ApplicationRecord
   enum status: { pending: 0, confirmed: 1 }
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def confirm_friend
+    Friendship.confirmed!
+    Friendship.create!(friend_id: user_id,
+                       user_id: friend_id,
+                       status: 'confirmed')
+  end
 end
